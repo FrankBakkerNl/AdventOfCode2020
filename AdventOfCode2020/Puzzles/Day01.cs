50dd518c78ef175a318e8839da97c666461d6076
Frank bakker <Frank.Bakker@avivasolutions.nl>
2020-12-04 14:15:02 +0100

    Day 1 with hashsets

--- AdventOfCode2020/Puzzles/Day01.cs
+++ AdventOfCode2020/Puzzles/Day01.cs
@@ -1,36 +1,29 @@
 ﻿using System;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace AdventOfCode2020.Puzzles
 {
+    /// <summary> https://adventofcode.com/2020/day/1 </summary>
     public class Day01
     {
         [Result(633216)]
         public static int GetAnswer1(int[] input)
         {
-            for (var i = 0; i < input.Length; i++)
-            {
-                for (var j = i+1; j < input.Length; j++)
-                {
-                    if (input[i] + input[j] == 2020) return input[i] * input[j];
-                }
-            }
-            throw new InvalidOperationException("No solution found");
+            var hashset = input.ToHashSet();
+            var x = hashset.First(x => hashset.Contains(2020 - x));
+
+            return x * (2020 - x );
         }
 
         [Result(68348924)]
         public static int GetAnswer2(int[] input)
         {
-            for (var i = 0; i < input.Length; i++)
-            {
-                for (var j = i + 1; j < input.Length; j++)
-                {
-                    for (var k = j + 1; k < input.Length; k++)
-                    {
-                        if (input[i] + input[j] + input[k] == 2020) return input[i] * input[j] * input[k];
-                    }
-                }
-            }
-            throw new InvalidOperationException("No solution found");
+            var hashset = input.ToHashSet();
+            var (xr,yr) = hashset.SelectMany(x => hashset.Where(y => y != x && x + y < 2020).Select(y => (x,y)))
+                .First(p=>hashset.Contains(2020 - p.x - p.y));
+
+            return xr * yr * (2020 - xr - yr);
         }
     }
 }
