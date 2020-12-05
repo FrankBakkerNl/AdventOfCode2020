3646237268316288d3b7f03589c680894c607481
Frank bakker <Frank.Bakker@avivasolutions.nl>
2020-12-05 21:31:42 +0100

    Day 4

--- AdventOfCode2020/Puzzles/Day01.cs
+++ AdventOfCode2020/Puzzles/Day01.cs
@@ -11,17 +11,17 @@ namespace AdventOfCode2020.Puzzles
         public static int GetAnswer1(int[] input)
         {
             var hashset = input.ToHashSet();
-            var x = hashset.First(x => hashset.Contains(2020 - x));
+            var xr = hashset.First(x => hashset.Contains(2020 - x));
 
-            return x * (2020 - x );
+            return xr * (2020 - xr);
         }
 
         [Result(68348924)]
         public static int GetAnswer2(int[] input)
         {
             var hashset = input.ToHashSet();
-            var (xr,yr) = input.SelectMany((x,i) => input.Skip(i).Where(y => x + y < 2020).Select(y => (x,y)))
-                .First(p=>hashset.Contains(2020 - p.x - p.y));
+            var (xr, yr) = input.SelectMany((x,i) => input[i..].Select(y => (x,y)))
+                .First(p => p.x + p.y <= 2020 && hashset.Contains(2020 - p.x - p.y));
 
             return xr * yr * (2020 - xr - yr);
         }
---
+++ AdventOfCode2020/Puzzles/Day04.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2020.Puzzles
+{
+    /// <summary> https://adventofcode.com/2020/day/4 </summary>
+    public class Day04
+    {
+        private static readonly string[] FieldNames = "byr iyr eyr hgt hcl ecl pid".Split(' ');
+
+        [Result(200)]
+        public static int GetAnswer1(string input)
+        {
+            var passports = ParsePassports(input);
+            return passports.Count(IsValid);
+        }
+
+        [Result(116)]
+        public static int GetAnswer2(string input)
+        {
+            var passports = ParsePassports(input);
+            return passports.Count(IsValidL2);
+        }
+
+        public static IEnumerable<IReadOnlyCollection<(string key, string value)>> ParsePassports(string input) => 
+            input.Split(new[] { Environment.NewLine + Environment.NewLine }, StringSplitOptions.None)
+                .Select(GetFields);
+
+        static IReadOnlyCollection<(string key, string value)> GetFields(string line) =>
+            Regex.Matches(line, @"(?<key>-?[a-z]+):(?<val>-?[^ \r]+)")
+                .Select(m => ( key:   m.Groups["key"].Value,
+                    value: m.Groups["val"].Value))
+                .Where(k => k.key != "cid").ToList();
+
+        static bool IsValid(IEnumerable<(string key, string value)> fields) => 
+            fields.Count() == 7;
+
+        static bool IsValidL2(IReadOnlyCollection<(string key, string value)> fields) =>
+            !FieldNames.Except(fields.Select(f => f.key)).Any() &&
+            fields.All(IsValidField);
+
+        /*
+byr (Birth Year) - four digits; at least 1920 and at most 2002.
+iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+hgt (Height) - a number followed by either cm or in:
+If cm, the number must be at least 150 and at most 193.
+If in, the number must be at least 59 and at most 76.
+hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+pid (Passport ID) - a nine-digit number, including leading zeroes.
+cid (Country ID) - ignored, missing or not.
+         */
+
+        static bool IsValidField((string key, string value) kv) =>
+            kv.key switch
+            {
+                "byr" => IsIntInRange(kv.value, 1920, 2002),
+                "iyr" => IsIntInRange(kv.value, 2010, 2020),
+                "eyr" => IsIntInRange(kv.value, 2020, 2030),
+                "hgt" => CheckHeight(kv.value),
+                "hcl" => Regex.IsMatch(kv.value, "^#[0-9a-f]{6}$"),
+                "ecl" => "amb blu brn gry grn hzl oth".Split(' ').Contains(kv.value),
+                "pid" => Regex.IsMatch(kv.value, "^[0-9]{9}$"),
+                "cid" => true,
+                _ => false
+            };
+
+        static bool IsIntInRange(string value, int min, int max) =>
+            int.TryParse(value, out var i) && min <= i && i <= max;
+
+        static bool CheckHeight(string value) =>
+            value.EndsWith("cm") && IsIntInRange(value[..^2], 150, 193) || 
+            value.EndsWith("in") && IsIntInRange(value[..^2], 59, 76);
+
+    }
+}
