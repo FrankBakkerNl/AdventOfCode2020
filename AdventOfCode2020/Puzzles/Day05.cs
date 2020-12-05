3aa4f220b7a2cf3847a26fae369b1d76dfa972de
Frank bakker <Frank.Bakker@avivasolutions.nl>
2020-12-05 21:41:37 +0100

    Day 5.1

---
+++ AdventOfCode2020/Puzzles/Day05.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2020.Puzzles
+{
+    /// <summary> https://adventofcode.com/2020/day/5 </summary>
+    public class Day05
+    {
+
+        [Focus]
+        public static int GetAnswer1(string []input)
+        {
+            return input.Max(GetSeatNumber);
+        }
+
+        public static int GetSeatNumber(string code)
+        {
+            return code.Select((c, i) => c == 'B' || c == 'R' ? 1 << 9-i : 0).Sum();
+        }
+    }
+}
---
+++ Tests/Puzzles/Day05test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using AdventOfCode2020.Puzzles;
+using FluentAssertions;
+using Xunit;
+
+namespace Tests.Puzzles
+{
+    public class Day05test
+    {
+
+        [Theory]
+        [InlineData("BFFFBBFRRR", 567)]
+        [InlineData("FFFBBBFRRR", 119)]
+        [InlineData("BBFFBBFRLL", 820)]
+
+        public void TestSeatNumber(string code, int expected)
+        {
+            Day05.GetSeatNumber(code).Should().Be(expected);
+        }
+    }
+}
