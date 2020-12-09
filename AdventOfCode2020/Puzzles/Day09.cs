620dc11d75e40e6e2da35d71b891a89cdaa912c6
Frank bakker <Frank.Bakker@avivasolutions.nl>
2020-12-09 08:57:31 +0100

    Day 9

---
+++ AdventOfCode2020/Puzzles/Day09.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2020.Puzzles
+{
+    /// <summary> https://adventofcode.com/2020/day/9 </summary>
+    public class Day09
+    {
+        [Result(18272118L)]
+        public static long GetAnswer1(long[] input) => FindFirstInvalid(input, 25);
+
+        public static long FindFirstInvalid(long[] input, int preSize)
+        {
+            var preceding = new Queue<long>(input.Take(preSize));
+
+            foreach (var current in input[preSize..])
+            {
+                if (!preceding.Any(x => preceding.Contains(current - x)))
+                    return current;
+
+                preceding.Enqueue(current);
+                preceding.Dequeue();
+            }
+            return -1;
+        }
+
+        [Result(2186361L)]
+        public static long GetAnswer2(long[] input) => FindRange(input, 18272118);
+
+        public static long FindRange(long[] input, long target)
+        {
+            int lower = 0;
+            int upper = 1;
+
+            long sum = input[lower] + input[upper];
+
+            while (sum != target)
+            {
+                if (sum < target)
+                {
+                    upper++;
+                    sum += input[upper];
+                }
+                else 
+                {
+                    sum -= input[lower];
+                    lower++;
+                }
+            }
+
+            var range = input[lower..upper];
+            return range.Max() + range.Min();
+        }
+    }
+}
---
+++ Tests/Puzzles/Day09test .cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using AdventOfCode2020.Puzzles;
+using FluentAssertions;
+using Xunit;
+using static System.Environment;
+
+namespace Tests.Puzzles
+{
+    public class Day09test
+    {
+        private long[] SampleData = @"35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576".Split(NewLine).Select(long.Parse).ToArray();
+
+        [Fact]
+        public void Answer1Test()
+        {
+            Day09.FindFirstInvalid(SampleData, 5).Should().Be(127);
+        }
+
+
+        [Fact]
+        public void Answer2Test()
+        {
+            Day09.FindRange(SampleData, 127).Should().Be(62);
+        }
+    }
+}
