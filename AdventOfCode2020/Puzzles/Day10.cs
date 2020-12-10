41d974a613efea45ff408e446391627ea7a1fea2
Frank bakker <Frank.Bakker@avivasolutions.nl>
2020-12-10 09:14:56 +0100

    Day 10

---
+++ AdventOfCode2020/Puzzles/Day10.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Reflection.Metadata.Ecma335;
+
+namespace AdventOfCode2020.Puzzles
+{
+    /// <summary> https://adventofcode.com/2020/day/9 </summary>
+    public class Day10
+    {
+        [Result(1656L)]
+        public static long GetAnswer1(int[] input)
+        {
+            var pairs = WithNext(input.OrderBy(i => i)).ToList();
+            var ones = pairs.Count(p => p.current == p.next - 1) + 1;
+            var trees = pairs.Count(p => p.current == p.next - 3) + 1;
+            return ones * trees;
+        }
+
+
+        private static IEnumerable<(T current, T next)> WithNext<T>(IEnumerable<T> input)
+        {
+            var previous = default(T);
+            var first = true;
+
+            foreach (var current in input)
+            {
+                if (!first)
+                {
+                    yield return (previous, current);
+                }
+
+                previous = current;
+                first = false;
+            }
+
+            yield return (previous, default(T));
+        }
+
+
+        [Result(56693912375296)]
+        public static long GetAnswer2(int[] input)
+        {
+            var hashset = input.ToHashSet();
+            hashset.Add(0);
+            var target = hashset.Max()+ 3;
+
+            var cache  = new Dictionary<int, long>();
+
+            long CountPaths(int target)
+            {
+                if (cache.TryGetValue(target, out var value)) return value;
+
+                if (target == 0 ) return 1;
+                long paths = 0;
+                if (hashset.Contains(target - 1)) paths += CountPaths(target - 1);
+                if (hashset.Contains(target - 2)) paths += CountPaths(target - 2);
+                if (hashset.Contains(target - 3)) paths += CountPaths(target - 3);
+                cache[target] = paths;
+                return paths;
+            }
+            
+            return CountPaths(target);
+        }
+    }
+}
---
+++ Tests/Puzzles/Day10test .cs
@@ -0,0 +1,74 @@
+﻿using System.Linq;
+using AdventOfCode2020.Puzzles;
+using FluentAssertions;
+using Xunit;
+using static System.Environment;
+
+namespace Tests.Puzzles
+{
+    public class Day10test
+    {
+        private int[] SampleData = @"16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4".Split(NewLine).Select(int.Parse).ToArray();
+
+        [Fact]
+        public void Answer1Test()
+        {
+            Day10.GetAnswer1(SampleData).Should().Be(35);
+        }
+
+
+        private int[] SampleData2 = @"28
+            33
+            18
+            42
+            31
+            14
+            46
+            20
+            48
+            47
+            24
+            23
+            49
+            45
+            19
+            38
+            39
+            11
+            1
+            32
+            25
+            35
+            8
+            17
+            7
+            9
+            4
+            2
+            34
+            10
+            3".Split(NewLine).Select(int.Parse).ToArray();
+
+        [Fact]
+        public void Answer2Test()
+        {
+            Day10.GetAnswer2(SampleData).Should().Be(8);
+        }
+
+        [Fact]
+        public void Answer2Test2()
+        {
+            Day10.GetAnswer2(SampleData2).Should().Be(19208);
+        }
+    }
+}
