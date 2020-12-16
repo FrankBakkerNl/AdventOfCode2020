6d6325657a40b1b2fc0dc5e006ef63c44a2c74c5
Frank bakker <Frank.Bakker@avivasolutions.nl>
2020-12-16 18:20:25 +0100

    Day 15

---
+++ AdventOfCode2020/Puzzles/Day15.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using static System.Linq.Enumerable;
+
+namespace AdventOfCode2020.Puzzles
+{
+    /// <summary> https://adventofcode.com/2020/day/15 </summary>
+    public class Day15
+    {
+//        [Focus]
+        [Result(1325L)]
+        public static long GetAnswer1(string input)
+        {
+            const int target = 2020;
+            return FindAnswerNoLong(input, target);
+        }
+
+
+        [Result(59006L)]
+        public static long GetAnswer2(string input)
+        {
+            const long target = 30_000_000;
+            return FindAnswerNoLong(input, target);
+        }
+
+        public static long FindAnswerNoLong(string input, long target)
+        {
+            var data = input.Split(',').Select(long.Parse).ToArray();
+
+            var mem = data[..^1].Select((v, i) => (v, i)).ToDictionary(t => t.v, t => (long)t.i + 1);
+            var lastSaid = data[^1];
+
+            for (var cur = data.Length; cur < target; cur++)
+            {
+                if (!mem.ContainsKey(lastSaid))
+                {
+                    mem[lastSaid] = cur;
+                    lastSaid = 0;
+                }
+                else
+                {
+                    var age = mem[lastSaid];
+                    mem[lastSaid] = cur;
+                    lastSaid = Math.Abs(cur - age);
+                }
+            }
+
+            return lastSaid;
+        }
+    }
+}
---
+++ Tests/Puzzles/Day15test.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using AdventOfCode2020.Puzzles;
+using FluentAssertions;
+using Xunit;
+using static System.Environment;
+
+namespace Tests.Puzzles
+{
+    public class Day15test
+    {
+        private string SampleData = @"0,3,6";
+
+        [Fact]
+        public void Answer1Test()
+        {
+            Day15.FindAnswerNoLong(SampleData, 10).Should().Be(0);
+        }
+
+        private string[] SampleData2 = @"".Split(NewLine);
+
+        [Fact]
+        public void Answer2Test()
+        {
+            Day15.FindAnswerNoLong(SampleData, 10).Should().Be(0);
+        }
+    }
+}
