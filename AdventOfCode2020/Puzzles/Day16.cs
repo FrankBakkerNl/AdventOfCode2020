99eb9160523bad25baddbde0c96fe03487d5d938
Frank bakker <Frank.Bakker@avivasolutions.nl>
2020-12-16 17:04:35 +0100

    Day 16.1

---
+++ AdventOfCode2020/Puzzles/Day16.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using static System.Linq.Enumerable;
+
+namespace AdventOfCode2020.Puzzles
+{
+    /// <summary> https://adventofcode.com/2020/day/16 </summary>
+    public class Day16
+    {
+        [Result(27870)]
+        public static int GetAnswer1(string input)
+        {
+            var ranges = GetRanges(input).ToArray();
+            var fieldvalue = TicketFields(input);
+
+            var invalid = fieldvalue.Where(v => !ranges.Any(r => IsInRange(v, r))).ToList();
+            return invalid.Sum();
+
+        }
+
+        static bool IsInRange(int value, (int, int) range) => value >= range.Item1 && value <= range.Item2;
+
+        public static IEnumerable<(int,int)> GetRanges(string input)
+        {
+            var matches = Regex.Matches(input, "(?<lower>[0-9]+)-(?<upper>[0-9]+)");
+            return matches.Select(m => (int.Parse(m.Groups["lower"].Value), int.Parse(m.Groups["upper"].Value)));
+        }
+
+        public static IEnumerable<int> TicketFields(string input)
+        {
+            var index = input.IndexOf("nearby tickets:");
+            var matches = Regex.Matches(input[index..], "[0-9]+");
+            return matches.Select(m => int.Parse(m.Value));
+
+        }
+    }
+}
---
+++ Tests/Puzzles/Day16test.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using AdventOfCode2020.Puzzles;
+using FluentAssertions;
+using Xunit;
+using static System.Environment;
+
+namespace Tests.Puzzles
+{
+    public class Day16test
+    {
+        private string SampleData = @"class: 1-3 or 5-7
+row: 6-11 or 33-44
+seat: 13-40 or 45-50
+
+your ticket:
+7,1,14
+
+nearby tickets:
+7,3,47
+40,4,50
+55,2,20
+38,6,12";
+
+        [Fact] public void PaseTest()
+        {
+            var ranges = Day16.GetRanges(SampleData);
+            var fields =  Day16.TicketFields(SampleData);
+        }
+
+        [Fact]
+        public void Answer1Test()
+        {
+            Day16.GetAnswer1(SampleData).Should().Be(71);
+        }
+
+        private string[] SampleData2 = @"".Split(NewLine);
+
+        [Fact]
+        public void Answer2Test()
+        {
+        }
+
+
+    }
+}
