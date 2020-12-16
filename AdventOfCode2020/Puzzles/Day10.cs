f5e23f70ae1045cd1507c5fe83b71d218d9aeadf
Frank bakker <Frank.Bakker@avivasolutions.nl>
2020-12-16 14:09:49 +0100

    Optimized + explained Day10.2

--- AdventOfCode2020/Puzzles/Day10.cs
+++ AdventOfCode2020/Puzzles/Day10.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 
 namespace AdventOfCode2020.Puzzles
 {
@@ -25,28 +26,37 @@ namespace AdventOfCode2020.Puzzles
         [Result(56693912375296)]
         public static long GetAnswer2(int[] input)
         {
-            var inputSet = input.Append(0).ToHashSet();
+            var adapterSet = input.ToHashSet();
 
-            var pathCounts = new long[input.Length  * 3 + 4 ];
-            pathCounts[0] = 1;
+            Span<long> pathCounts = stackalloc long[] {1L, 1L, 1L}; 
+            // Ring buffer for 3 path counts we need to track
+            // Because we start at 0 we know there is at least 1 path to values 1, 2, and 3
 
-            var skipped = 0;
+            var lastValue = 0;
+            var numPaths = 1L;
 
-            for (var i = 0; ; i++)
+            // Loop all possible adapter values from 1 until we missed 3 values in a row which must be the end
+            for (var i = 1; i <= lastValue + 3 ; i++)
             {
-                if (!inputSet.Contains(i))
+                if (!adapterSet.Contains(i))
                 {
-                    if (++skipped == 3) return pathCounts[i - skipped]; // If we skipped 3 times in a row we are passed the end
+                    // No adapter for this value, so no paths lead here 
+                    pathCounts[i % 3] = 0;
                     continue;
                 }
+                lastValue = i;
 
-                var paths = pathCounts[i];
-                pathCounts[i + 1] += paths;
-                pathCounts[i + 2] += paths;
-                pathCounts[i + 3] += paths;
+                numPaths = pathCounts[i % 3];
 
-                skipped = 0;
+                // Increment number of paths to the next two values with the current number of paths to this adapter
+                pathCounts[(i + 1) % 3] += numPaths;
+                pathCounts[(i + 2) % 3] += numPaths;
+                // i + 3 starts with the same number of paths as the current, 
+                // however we do not need to update the value because the same slot in the ring buffer will be re-used 
+                // pathCounts[(i + 3) % 3] is the same as pathCounts[i % 3]
             }
+
+            return numPaths;
         }
     }
 }
